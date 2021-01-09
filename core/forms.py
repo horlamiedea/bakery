031ef9ee297a382183919fb4899520726c7ad379
horlamiedea <horlamiedea@gmail.com>
2021-01-09 18:31:44 +0100

    awaiting design

---
+++ core/forms.py
@@ -0,0 +1,86 @@
+from django import forms
+from django_countries.fields import CountryField
+from django_countries.widgets import CountrySelectWidget
+
+
+PAYMENT_CHOICES = (
+    ('S', 'Stripe'),
+    ('P', 'PayPal')
+)
+SHAPE = (
+    ('R', 'Round'),
+    ('Re', 'Rectangle'),
+    ('S', 'Square'),
+    ('C', 'Custom')
+    
+)
+
+LEVELS = (
+    (1, 'One'),
+    (2, 'Two'),
+    (3, 'Three'),
+    (4, 'Four')
+)
+
+FLAVORS = (
+    ('C', 'Chocolate'),
+    ('S', 'Strawberry'),
+    ('V', 'Vanilla')
+)
+
+class HomeForm(forms.Form):
+    shape = forms.ChoiceField(, choices=[SHAPE] ,widget=forms.RadioSelect, required=False)
+    levels = forms.ChoiceField(, choices=[LEVELS] ,widget=forms.RadioSelect, required=False)
+    flavors = forms.ChoiceField(, choices=[FLAVORS] ,widget=forms.RadioSelect, required=False)
+
+
+class CheckoutForm(forms.Form):
+    shipping_address = forms.CharField(required=False)
+    shipping_address2 = forms.CharField(required=False)
+    shipping_country = CountryField(blank_label='(select country)').formfield(
+        required=False,
+        widget=CountrySelectWidget(attrs={
+            'class': 'custom-select d-block w-100',
+        }))
+    shipping_zip = forms.CharField(required=False)
+
+    billing_address = forms.CharField(required=False)
+    billing_address2 = forms.CharField(required=False)
+    billing_country = CountryField(blank_label='(select country)').formfield(
+        required=False,
+        widget=CountrySelectWidget(attrs={
+            'class': 'custom-select d-block w-100',
+        }))
+    billing_zip = forms.CharField(required=False)
+
+    same_billing_address = forms.BooleanField(required=False)
+    set_default_shipping = forms.BooleanField(required=False)
+    use_default_shipping = forms.BooleanField(required=False)
+    set_default_billing = forms.BooleanField(required=False)
+    use_default_billing = forms.BooleanField(required=False)
+
+    payment_option = forms.ChoiceField(
+        widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
+
+
+class CouponForm(forms.Form):
+    code = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Promo code',
+        'aria-label': 'Recipient\'s username',
+        'aria-describedby': 'basic-addon2'
+    }))
+
+
+class RefundForm(forms.Form):
+    ref_code = forms.CharField()
+    message = forms.CharField(widget=forms.Textarea(attrs={
+        'rows': 4
+    }))
+    email = forms.EmailField()
+
+
+class PaymentForm(forms.Form):
+    stripeToken = forms.CharField(required=False)
+    save = forms.BooleanField(required=False)
+    use_default = forms.BooleanField(required=False)
